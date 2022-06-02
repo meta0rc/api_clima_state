60207411c31723224ade348ba06b651b51d5af64
meta0rc <70641868+meta0rc@users.noreply.github.com>
2022-06-02 18:44:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.post('/search', async (req, res) => {
         //     
         await browser.close();     
         
-        return res.status(200).json(clima)
+        return res.status(200).json({tempo: clima})
         
     })();
     
